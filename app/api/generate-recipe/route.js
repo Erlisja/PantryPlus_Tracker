199c082764d54d19486dd5817e302ab3a246fe43
Erlisja Kore <korelisa@yahoo.com>
2024-08-07 01:11:53 -0400

    Update route.js

--- app/api/generate-recipe/route.js
+++ app/api/generate-recipe/route.js
@@ -1,27 +1,4 @@
 
-// import  {generateRecipe, generateRecipeImage } from '@/app/openaiClient';
-// import { NextResponse } from 'next/server';
-
-// export async function POST(request) {
-//   try {
-//     const { ingredients } = await request.json();
-
-//     if (!ingredients || !Array.isArray(ingredients) || ingredients.length === 0) {
-//       return NextResponse.json({ error: 'Ingredients list cannot be empty' }, { status: 400 });
-//     }
-
-//     const recipe = await generateRecipe(ingredients);
-//     // Generate recipe image
-//     const imageUrl = await generateRecipeImage(ingredients);
-
-//     console.log('Generated Recipe:', recipe); // Log the recipe for debugging
-
-//     return NextResponse.json({ recipe });
-//   } catch (error) {
-//     console.error('Error generating recipe:', error);
-//     return NextResponse.json({ error: 'An error occurred while generating the recipe' }, { status: 500 });
-//   }
-// }
 import { generateRecipe, generateRecipeImage } from '@/app/openaiClient';
 
 // Export a named function for the POST method
